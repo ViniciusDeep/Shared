fb34dc49dbd11a2af63d040c6977a49c6da7bc7c
Vinicius <viniciuscorreia11@gmail.com>
2017-11-13 18:06:08 -0300

    Alter views on sign up and login. Add buttons, sign-up to viewController

---
+++ Challenger/Controllers/SingUpViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SingUpViewController.swift
+//  Challenger
+//
+//  Created by Vinicius Mangueira Correia on 13/11/17.
+//  Copyright © 2017 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import UIKit
+
+class SingUpViewController: UIViewController {
+
+   
+    
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    @IBAction func BackButton(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil)
+        }
+    
+    
+    @IBAction func signButton(_ sender: Any) {
+    }
+    
+    
+    
+}
