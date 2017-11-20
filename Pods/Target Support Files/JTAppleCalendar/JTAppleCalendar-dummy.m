5df2df7c5e19cdfe707174a76403c4c5334831c4
Vinicius <viniciuscorreia11@gmail.com>
2017-11-20 14:27:19 -0300

    Remove podfile.lock

--- Pods/Target Support Files/JTAppleCalendar/JTAppleCalendar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JTAppleCalendar : NSObject
-@end
-@implementation PodsDummy_JTAppleCalendar
-@end
--- Pods/Target Support Files/Pods-Challenger/Pods-Challenger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Challenger : NSObject
-@end
-@implementation PodsDummy_Pods_Challenger
-@end
