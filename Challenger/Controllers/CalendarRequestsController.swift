f069a000ae569276de6ced9fc81453f605cecc1e
yagosaboia <yagosaboia@gmail.com>
2017-11-23 14:40:28 -0300

    Start the cutting

---
+++ Challenger/Controllers/CalendarRequestsController.swift
@@ -0,0 +1,25 @@
+//
+//  CalendarRequestsController.swift
+//  Challenger
+//
+//  Created by Yago Saboia Felix Frota on 23/11/17.
+//  Copyright © 2017 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalendarRequestController: UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
